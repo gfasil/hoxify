c9893ca94682c23bd9131f85bf8f97a453f54897
gfasil <fhabtegiorgis@mum.edu>
2020-08-16 18:55:19 -0500

    basic form test impelemented
    basic form built

--- src/frontend/src/pages/signup.js
+++ src/frontend/src/pages/signup.js
@@ -1,12 +1,25 @@
 import React,{Component} from 'react';
    
-export default class signup extends Component{
+ export default class signup extends Component{
  
         render(){
 
             return (
                 <div>
                     <h1>sign up</h1>
+                    <div>
+                    <input placeholder="your display name"/>
+                    </div>
+                    <div>
+                    <input placeholder="your user name"/>
+                    </div>
+                    <div>
+                    <input placeholder="your password" type="password"/>
+                    </div>
+                    <div>
+                    <input placeholder="repeat your password" type="password"/>
+                    </div>
+                   <button>submit</button>
                 </div>)
         }
     
--- src/frontend/src/pages/signup.spec.js
+++ src/frontend/src/pages/signup.spec.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import {render} from '@testing-library/react';
+import {render, cleanup} from '@testing-library/react';
  import '@testing-library/jest-dom/extend-expect';
 import Signup from './signup'
 
+beforeEach(cleanup);
 describe('SignUp',()=>{
 
     describe('Layout',()=>{
@@ -11,6 +12,49 @@ describe('SignUp',()=>{
             const {container}=render(<Signup/>);
             const header= container.querySelector('h1');
             expect(header).toHaveTextContent('sign up')
+             
+       
+        })
+        it('has input field for display name',()=>{
+            const {queryByPlaceholderText}=render(<Signup/>)
+            const displayNameInput=queryByPlaceholderText('your display name')
+            expect(displayNameInput).toBeInTheDocument();
+
+        })
+        it('has input field for user name',()=>{
+            const {queryByPlaceholderText}=render(<Signup/>)
+            const displayNameInput=queryByPlaceholderText('your user name')
+            expect(displayNameInput).toBeInTheDocument();
+
+        })
+        it('has input field for password',()=>{
+            const {queryByPlaceholderText}=render(<Signup/>)
+            const displayNameInput=queryByPlaceholderText('your password')
+            expect(displayNameInput).toBeInTheDocument();
+
+        })
+        it('has input field for password',()=>{
+            const {queryByPlaceholderText}=render(<Signup/>)
+            const displayNameInput=queryByPlaceholderText('your password')
+            expect(displayNameInput.type).toBe('password');
+
+        })
+        it('has input field for repeat password',()=>{
+            const {queryByPlaceholderText}=render(<Signup/>)
+            const displayNameInput=queryByPlaceholderText('repeat your password')
+            expect(displayNameInput).toBeInTheDocument();
+
+        })
+        it('has input field for password',()=>{
+            const {queryByPlaceholderText}=render(<Signup/>)
+            const displayNameInput=queryByPlaceholderText('repeat your password')
+            expect(displayNameInput.type).toBe('password');
+
+        })
+        it('has header of sign up',()=>{
+            const {container}=render(<Signup/>);
+            const header= container.querySelector('button');
+            expect(header).toHaveTextContent('submit')
              
        
         })
